63672a9bed6d14aaf068629b2be79a1c4572ae6c
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 18:58:05 -0400

    Hash Utils

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,15 @@
 import { StatusCodes } from 'http-status-codes';
-import bcrypt from 'bcryptjs';
 // Models
 import User from '../models/UserModel.js';
+// Utils
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
 
   // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
