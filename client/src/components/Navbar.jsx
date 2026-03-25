92b8345054a45ca1f6988a1deae1068c940634b5
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 17:28:07 -0400

    Navbar Structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,29 @@
-import { FaHome } from 'react-icons/fa';
+import { FaAlignLeft } from 'react-icons/fa';
+// Custom hook (Context)
+import { useDashboardContext } from '../pages/DashboardLayout';
 // Wrappers
 import Wrapper from '../assets/wrappers/Navbar';
+// Components
+import Logo from './Logo';
 
 const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
+
   return (
     <Wrapper>
-      <h2>Navbar</h2>
-      <FaHome />
+      <div className="nav-center">
+        {/* TOGGLE SIDEBAR */}
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        {/* LOGO */}
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        {/* LOGOUT */}
+        <div className="btn-container">toggle/logout</div>
+      </div>
     </Wrapper>
   );
 };
