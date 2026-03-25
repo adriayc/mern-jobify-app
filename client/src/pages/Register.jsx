a57cd2b060344abe8a591c97000b8215451bf012
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 17:23:50 -0400

    Register Page - Setup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,39 @@
 import { Link } from 'react-router-dom';
+// Wrappers
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+// Components;
+import { Logo } from '../components';
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue="adriano"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
