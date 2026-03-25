72759ec3c1af5943fc5fb44516f81354e91f4a03
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 17:15:20 -0400

    Nested Routes

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <h1>HomeLayout</h1>;
+  return (
+    <div>
+      <nav>navbar</nav>
+      {/* Outlet */}
+      <Outlet />
+    </div>
+  );
 };
 
 export default HomeLayout;
