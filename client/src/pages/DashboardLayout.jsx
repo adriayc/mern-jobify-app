6858c99cca9d0827fcf0362698ccd5846681b771
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 17:27:22 -0400

    Dashboard Setup

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 const DashboardLayout = () => {
-  return <h1>DashboardLayout Page</h1>;
+  return (
+    <div>
+      {/* OUTLET */}
+      <Outlet />
+    </div>
+  );
 };
 
 export default DashboardLayout;
