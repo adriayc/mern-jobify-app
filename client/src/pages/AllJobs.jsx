40d404840bd1166914f8e2460071ada8496217db
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:46:10 -0400

    Job Component CSS

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -23,7 +23,7 @@ const AllJobsContext = createContext();
 
 const AllJobs = () => {
   const { data } = useLoaderData();
-  console.log(data);
+  // console.log(data);
 
   return (
     <AllJobsContext.Provider value={{ data }}>
