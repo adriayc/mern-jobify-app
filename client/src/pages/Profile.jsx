ad7f649698a589ed4e1dc89c00dfa1dd4bae70e0
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:52:28 -0400

    Profile Action

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,28 @@ import { Form, useNavigation, useOutletContext } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 // Components
 import FormRow from '../components/FormRow';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+// Actions
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+
+  const file = formData.get('avatar');
+  if (file && file.size > 500000) {
+    toast.error('Image size too large');
+    return null;
+  }
+
+  try {
+    await customFetch.patch('/users/update-user', formData);
+    toast.success('Profile updated successfully');
+  } catch (error) {
+    // console.log(error);
+    toast.error(error?.response?.data?.msg);
+  }
+  return null;
+};
 
 const Profile = () => {
   const { user } = useOutletContext();
