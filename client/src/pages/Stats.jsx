45bafc9716f277e5378b604122f7bb5f3cf996cf
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:56:26 -0400

    Stats Page Setup

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,5 +1,30 @@
+import { useLoaderData } from 'react-router-dom';
+// Custom fetch
+import customFetch from '../utils/customFetch';
+import { ChartsContainer, StatsContainer } from '../components';
+
+// Loaders
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/jobs/stats');
+    return response.data;
+  } catch (error) {
+    // console.log(error);
+    return error;
+  }
+};
+
 const Stats = () => {
-  return <h1>Stats Page</h1>;
+  const { defaultStats, monthlyApplications } = useLoaderData();
+
+  return (
+    <>
+      <StatsContainer defaultStats={defaultStats} />
+      {monthlyApplications?.length > 1 && (
+        <ChartsContainer data={monthlyApplications} />
+      )}
+    </>
+  );
 };
 
 export default Stats;
