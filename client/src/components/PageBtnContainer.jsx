6f651cbc61c6139fcc3f4ddafa9a77d86af7b83c
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-26 00:01:13 -0400

    Render Buttons

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,3 +1,4 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 // Wrappers
 import Wrapper from '../assets/wrappers/PageBtnContainer';
 // Custom hooks
@@ -7,9 +8,36 @@ const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
-  console.log(numOfPages, currentPage);
 
-  return <Wrapper>PageBtnContainer</Wrapper>;
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+  //   console.log(pages);
+
+  return (
+    <Wrapper>
+      <button className="btn prev-btn">
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className="btn next-btn">
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 };
 
 export default PageBtnContainer;
