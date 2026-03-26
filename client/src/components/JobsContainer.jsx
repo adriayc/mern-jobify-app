b3f968effa6d91f2910763beca9b3b37ccb1cff5
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:45:20 -0400

    Jobs Container JSX

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,5 +1,31 @@
+// Wrappers
+import Wrapper from '../assets/wrappers/JobsContainer';
+// Custom hook
+import { useAllJobsContext } from '../pages/AllJobs';
+// Components
+import Job from './Job';
+
 const JobContainer = () => {
-  return <h2>JobContainer</h2>;
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobContainer;
