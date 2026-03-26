188611520ff504bb03f435247fda6de4dc5bdd0a
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:49:48 -0400

    Admin Page Setup

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,31 @@
+import { redirect, useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
+// Wrappers
+import Wrapper from '../assets/wrappers/StatsContainer';
+// Custom fetch
+import customFetch from '../utils/customFetch';
+
+// Loaders
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/users/admin/app-stats');
+    return response.data;
+  } catch (error) {
+    // console.log(error);
+    toast.error('You are not authorized to view this page');
+    return redirect('/dashboard');
+  }
+};
+
 const Admin = () => {
-  return <h1>Admin Page</h1>;
+  const { users, jobs } = useLoaderData();
+  console.log(users, jobs);
+
+  return (
+    <Wrapper>
+      <h1>Admin Page</h1>
+    </Wrapper>
+  );
 };
 
 export default Admin;
