d566d9bae474e51d7f25b995dc999ec895054c9a
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:49:58 -0400

    Admin Page Complete

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,9 +1,12 @@
 import { redirect, useLoaderData } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { FaCalendarCheck, FaSuitcaseRolling } from 'react-icons/fa';
 // Wrappers
 import Wrapper from '../assets/wrappers/StatsContainer';
 // Custom fetch
 import customFetch from '../utils/customFetch';
+// Components
+import { StatItem } from '../components';
 
 // Loaders
 export const loader = async () => {
@@ -19,11 +22,23 @@ export const loader = async () => {
 
 const Admin = () => {
   const { users, jobs } = useLoaderData();
-  console.log(users, jobs);
 
   return (
     <Wrapper>
-      <h1>Admin Page</h1>
+      <StatItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
     </Wrapper>
   );
 };
