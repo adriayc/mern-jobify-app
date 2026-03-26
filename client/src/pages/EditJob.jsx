d1ba32b30fb2d0c912c2a0e098c2f1375d923b5e
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:48:02 -0400

    Edit Job Loader

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,11 +1,24 @@
-import { useParams } from 'react-router-dom';
+import {
+  redirect,
+  useLoaderData,
+  //useParams
+} from 'react-router-dom';
+import { toast } from 'react-toastify';
 // Wrappers
 import Wrapper from '../assets/wrappers/DashboardFormPage';
+// Custom fetch
+import customFetch from '../utils/customFetch';
 
 // Loaders
-export const loader = ({ params }) => {
-  console.log(params);
-  return null;
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    // console.log(error);
+    toast.error(error?.response?.data?.msg);
+    return redirect('/dashboard/all-jobs');
+  }
 };
 // Actions
 export const action = () => {
@@ -13,8 +26,10 @@ export const action = () => {
 };
 
 const EditJob = () => {
-  const params = useParams();
-  console.log(params);
+  // const params = useParams();
+  // console.log(params);
+  const { job } = useLoaderData();
+  console.log(job);
 
   return <h1>EditJob Page</h1>;
 };
