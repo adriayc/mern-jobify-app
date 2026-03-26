14ab8b70033f01be564088ad9989eccf8c5ee129
Adriano Ayala <adriano.ayc@gmail.com>
2026-03-25 23:48:35 -0400

    Edit Job Action

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -27,8 +27,20 @@ export const loader = async ({ params }) => {
   }
 };
 // Actions
-export const action = () => {
-  return null;
+export const action = async ({ request, params }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.patch(`/jobs/${params.id}`, data);
+    toast.success('Job edited successfully');
+
+    return redirect('/dashboard/all-jobs');
+  } catch (error) {
+    // console.log(error);
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
 };
 
 const EditJob = () => {
